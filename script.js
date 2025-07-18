5a5bf8e8843839287daeea7cb99d46ab6d7c1c2a
naveen-111q <nk651558@gmail.com>
2025-07-18 07:45:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showContact() {
+  const info = document.getElementById("contactInfo");
+  if (info.style.display === "none") {
+    info.style.display = "block";
+  } else {
+    info.style.display = "none";
+  }
+}
